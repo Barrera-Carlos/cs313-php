f111f978a0994429d250e165b684103655c49705
Carlosz31 <bar12033@byui.edu>
2017-09-23 22:34:07 -0500

    02 Prove

--- web/mainPageScript.js
+++ web/mainPageScript.js
@@ -1,3 +1,11 @@
 /*jquwey functions need to be inside a document ready event
 so the hquery code does not run before the document if fininidhed
 loading*/
+$(document).ready(function(){
+    $("img").mouseover(function(){
+        $("#face").attr("src", "imgP/fFace.jpg");
+    });
+    $("img").mouseout(function(){
+        $("#face").attr("src", "imgP/sFace.jpg");
+    });
+});
