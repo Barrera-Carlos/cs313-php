b870d154fa915ca626d3c677a45731eadeacc9ea
Carlosz31 <bar12033@byui.edu>
2017-09-29 07:02:57 -0500

    rest of the shop

---
+++ web/Prove/shop.js
@@ -0,0 +1,5 @@
+function changeDisplay(){
+  //document.getElementById("img").style.display = "none";
+  var element = document.getElementsByClassName("col-centered");
+  element.style.display = null;
+}
