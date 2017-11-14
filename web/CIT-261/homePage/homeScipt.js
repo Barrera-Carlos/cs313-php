c2f71ab1cd9787f375892af0b74fea3d3c5117c3
Carlos Barrera <bar12033@byui.edu>
2017-11-13 22:59:53 -0600

    added a btn down event

--- web/CIT-261/homePage/homeScipt.js
+++ web/CIT-261/homePage/homeScipt.js
@@ -1,29 +1,66 @@
-function firstPage() {
+var bodyImgElm;
+var portrait;
+var aboutMe;
+var spaceCount;
+var hasBodyImgChange = false;
+var hasPortraitImgChange = false;
 
-    var title = document.getElementById("on");
-    title.style.backgroundColor = "#ff3333";
+function keyDown() {
+    spaceCount += 1;
+    switch (spaceCount){
+        case 1:
+            var title = document.getElementById("on");
+            title.style.backgroundColor = "#ff3333";
 
-    var elm = document.getElementsByClassName("navBar");
-    for(i = 0; i < elm.length; i++){
-        elm[i].style.display = "block";
-        if(elm[i] !== title)
-            elm[i].style.color = "black";
+            var navBarelm = document.getElementsByClassName("navBar");
+            for(var i = 0; i < navBarelm.length; i++){
+                navBarelm[i].style.display = "block";
+                if(navBarelm[i] !== title)
+                    navBarelm[i].style.color = "black";
+            }
+
+            var ul = document.getElementById("myUL");
+            ul.style.overflow ="auto";
+            ul.style.height = "100%";
+            ul.style.position = "fixed";
+            ul.style.width = "25%";
+            ul.style.backgroundColor = "white";
+
+            break;
+        case 2:
+            if(!hasPortraitImgChange)
+                changePortraitImg();
+            break;
+        case 3:
+            if(!hasBodyImgChange)
+                changeBodyImg();
+            break;
+
     }
 
-    var ul = document.getElementById("myUL");
-    ul.style.overflow ="auto";
-    ul.style.height = "100%";
-    ul.style.position = "fixed";
-    ul.style.width = "25%";
-    ul.style.backgroundColor = "white";
+}
 
-    var title = document.getElementById("on");
-    title.style.backgroundColor = "#ff3333";
-
-    document.getElementsByClassName("Me")[1].src = "fFace.jpg";
-    var aboutMe = document.getElementsByClassName("aboutMe")[0];
+function changeBodyImg() {
+    hasBodyImgChange = true;
+    bodyImgElm.style.backgroundImage = "url(simple.jpg)";
     aboutMe.style.backgroundColor = "#ff3333";
     aboutMe.style.border = "15px solid lightblue";
     aboutMe.style.color = "white";
-    document.getElementsByTagName("BODY")[0].style.backgroundImage = "url(simple.jpg)";
+
+}
+
+function changePortraitImg() {
+    hasPortraitImgChange = true;
+    portrait.src = "fFace.jpg";
+}
+
+function loadElm() {
+    spaceCount = 0;
+    bodyImgElm = document.getElementsByTagName("BODY")[0];
+    aboutMe = document.getElementsByClassName("aboutMe")[0];
+    aboutMe.addEventListener('touchend',changeBodyImg, false);
+    portrait = document.getElementsByClassName("Me")[1];
+    portrait.addEventListener('touchend',changePortraitImg, false);
+
+    document.addEventListener("keydown", keyDown, false);
 }
