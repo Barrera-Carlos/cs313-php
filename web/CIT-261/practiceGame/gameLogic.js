27f2d156e1b7e4d70198981a419d1be89e150581
Carlos Barrera <bar12033@byui.edu>
2017-11-30 15:22:10 -0600

    log in pop up working correctly

--- web/CIT-261/practiceGame/gameLogic.js
+++ web/CIT-261/practiceGame/gameLogic.js
@@ -1,45 +1,6 @@
 ////////////////////////////////////////////////////////////////////
 //this code is used to log-in using Facebook
 
-// This code is used to enable users to store there information using there Facebook login
-window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '{1960623427485728}',
-        cookie     : true,  // enable cookies to allow the server to access
-                            // the session
-        xfbml      : true,  // parse social plugins on this page
-        version    : 'v2.8' // use graph api version 2.8
-    });
-
-    // Now that we've initialized the JavaScript SDK, we call
-    // FB.getLoginStatus().  This function gets the state of the
-    // person visiting this page and can return one of three states to
-    // the callback you provide.  They can be:
-    //
-    // 1. Logged into your app ('connected')
-    // 2. Logged into Facebook, but not your app ('not_authorized')
-    // 3. Not logged into Facebook and can't tell if they are logged into
-    //    your app or not.
-    //
-    // These three cases are handled in the callback function.
-
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-    });
-
-};
-
-// Load the SDK asynchronously
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
-
 
 // This is called with the results from from FB.getLoginStatus().
 function statusChangeCallback(response) {
@@ -334,8 +295,6 @@ function setTheme() {
 }*/
 
 function setDeck() {
-
-
     var count = 0;
     var shuffled = shuffleLocation();
 
