9d7477ed3d2b22e26ab7059cad0e84e337bca709
Carlos Barrera <bar12033@byui.edu>
2017-12-05 12:28:20 -0600

    cleanning up code

--- web/CIT-261/practiceGame/flipLogic.js
+++ web/CIT-261/practiceGame/flipLogic.js
@@ -101,6 +101,7 @@ function logInButton() {
 }
 
 function displayPlayerStats(){
+    alert("new display");
     document.getElementById("stats").innerHTML = "Average Time: "+ String(Math.round(playerStats.timeAverageMean()))
         + 's'+ "<br>"+ "Recent time: " + String(playerStats.recentGameTime()) + 's' + "<br>" +
         "Clicks: " + String(playerStats.clicks);
